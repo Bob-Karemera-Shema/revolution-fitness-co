43abbff601cbb843098f4fbabbc71aaf47a05678
bob-karemera <bob.karemera@amalitech.com>
2026-05-19 17:42:53 +0200

    feat: hero section skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <main></main>
+    <main>
+      <section>
+        <article className="flex items-center justify-center uppercase font-bold text-5xl md:text-8xl text-center py-8 border-b border-divider-gray">
+          <p>Achieve your health and fitness goals</p>
+        </article>
+        <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          <div></div>
+
+          <div></div>
+        </article>
+      </section>
+    </main>
   );
 }
